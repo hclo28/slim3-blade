ca19067fda2a51ab0b9930785252330c917c7c71
hana <>
2019-07-15 11:03:13 +0900

    delete unnecessary comment

--- app/index.php
+++ app/index.php
@@ -4,15 +4,3 @@ require 'vendor/autoload.php';
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/Controller/SampleController.php';
 require __DIR__ . '/Config/Routes.php';
-
-// // Create Slim app
-// $app = new \Slim\App;
-
-// // Fetch DI Container
-// $container = $app->getContainer();
-
-// $app->get('/', function ( $request, $response ) {
-//     return $response->write('Slim is running...');
-// });
-
-// $app->run();
